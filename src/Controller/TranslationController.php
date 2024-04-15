4051b6d7d207ceccb5756746ae20868ac08b9d65
getcontext <andrzej.salamon@gmail.com>
2024-04-15 15:59:32 +0200

    service + inject

--- src/Controller/TranslationController.php
+++ src/Controller/TranslationController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Guide;
+use App\Service\TranslationService;
+use App\Service\TranslationService\ExternalTranslationService;
+use Symfony\Bridge\Doctrine\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,12 +13,31 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/api/v1', name: 'api_v1_')]
 class TranslationController extends AbstractController
 {
-    #[Route('/translation', name: 'app_translation')]
-    public function index(): JsonResponse
+    private TranslationService  $translationService;
+
+    /**
+     * @param TranslationService $translationService
+     */
+    public function __construct(TranslationService $translationService)
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/TranslationController.php',
-        ]);
+        $this->translationService = $translationService;
+    }
+
+    #[Route('/auto-translate/{id}', name: 'translation_index', methods:['get'] )]
+    public function index(ManagerRegistry $doctrine, int $id): JsonResponse
+    {
+        $entity = $doctrine->getRepository(Guide::class)->find($id);
+
+        if (!$entity) {
+            return $this->json('No guide found for id ' . $id, 404);
+        }
+
+        $data =  [
+            'id' => $entity->getId(),
+            'name' => $entity->getName(),
+            'description' => $entity->getDescription(),
+        ];
+
+        return $this->json($data);
     }
 }
