f397eb22f35812d5228d98e8bab4ca9cd2fe556a
getcontext <andrzej.salamon@gmail.com>
2024-04-15 13:04:16 +0200

    route api to controller added

--- src/Controller/TranslationController.php
+++ src/Controller/TranslationController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/api/v1', name: 'api_v1_')]
 class TranslationController extends AbstractController
 {
     #[Route('/translation', name: 'app_translation')]
